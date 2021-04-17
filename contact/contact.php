e9b5422b47d510394c224b48888b9b7a4e02eedb
Dariwka <77488950+Dariwka@users.noreply.github.com>
2021-04-17 19:44:58 +0300

    new task formatter of pokemons

--- contact/contact.php
+++ contact/contact.php
@@ -1,5 +1,21 @@
 <?php 
-echo '<pre>';
+if(isset($_POST['name'])&& isset($_POST['email'])&& filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+$to = 's2100157@edu.bc.fi';
+$subject = 'User' . $_POST['name'] . 'with email' . $_POST['email'] . 'has sent me an email';
+$message = $_POST['message'];
+
+$success = mail($to. $subject, $message);
+
+if(!$success){
+    echo 'an error has occured while sending the email';
+} else {
+    echo ' email was sent';
+}
+} else {
+    echo ' invalid input';
+}
+
+/*echo '<pre>';
 print_r($_POST);
 echo '</pre>';
 $sent_status = mail($_POST['email'], 'Sent from test PHP application', ($_POST['message'] . ' sent by' . $_POST['user']));
@@ -8,5 +24,5 @@ if($sent_status){
     echo 'Message was delivered';
 }else{
     echo 'Email was not sent';
-}
+}*/
 ?>
